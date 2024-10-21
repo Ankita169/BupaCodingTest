db593a91468a4f2a1cf04fa57c120e8183f61bd6
Ankita Srivastava <ankita.srivastava06@ad.infosys.com>
2024-10-21 17:30:45 +0530

    AddTestCases

---
+++ BookOwnerTest/BookOwnerTests.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using BookOwner.Controllers;
+using BookOwner.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace BookOwnerTest
+{
+    public class Tests
+    {
+        private HomeController _homeController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _homeController = new HomeController();
+        }
+
+        [Test]
+        public async Task TestHomeControllerBook()
+        {
+            var expectedOwner = new Owner
+            {
+                Name="Ankita",
+                Age= 23,
+                OwnerAdult = new List<book>
+
+                { 
+
+                new book { Name = "Book 1",Type="Hardcover" },
+
+                new book { Name = "Book 2", Type="PaperCover"},
+                
+                },
+                OwnerChild = new List<book>
+
+                {
+
+                new book { Name = "Book 3",Type="Hardcover"},
+
+                new book { Name = "Book 4" ,Type="PaperCover"},
+               
+
+                },
+                Books = new List<book>
+
+                {
+
+                new book { Name = "Book 1", Type = "HardCover" },
+
+                new book { Name = "Book 2", Type = "Paper" },
+
+               
+                }
+            };
+            var result = await _homeController.Book() as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.AreEqual(expectedOwner.Name, ((Owner)result.Model).Name);
+            Assert.AreEqual(expectedOwner.Age, ((Owner)result.Model).Age);
+            CollectionAssert.AreEqual(expectedOwner.OwnerAdult, ((Owner)result.Model).OwnerAdult);
+
+            CollectionAssert.AreEqual(expectedOwner.OwnerChild, ((Owner)result.Model).OwnerChild);
+
+            CollectionAssert.AreEqual(expectedOwner.Books, ((Owner)result.Model).Books);
+        }
+
+        [Test]
+        public async Task TesthomeControllerError()
+        {
+            var expectedViewName = "Error";
+
+            var result =  _homeController.Error() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.AreEqual(expectedViewName, result.ViewName);
+        }
+        public async Task TesthomeControllerAllBook()
+        {
+            var expectedViewName = "AllBook";
+
+            var result = _homeController.Error() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.AreEqual(expectedViewName, result.ViewName);
+        }
+
+        public async Task TesthomeControllerHardCoverBooks()
+        {
+            var expectedViewName = "HardCoverBooks";
+
+            var result = _homeController.Error() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.AreEqual(expectedViewName, result.ViewName);
+        }
+    }
+}
---
+++ BookOwnerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BookOwnerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BookOwnerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4718c092-93ba-43b2-9ebd-ce741103ecdc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
