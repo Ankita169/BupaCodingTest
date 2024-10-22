c26756088b9c890a177c662f222b089e485dfce0
Ankita169 <ankita.srivastava06@infosys.com>
2024-10-22 15:45:45 +0530

    Update AssemblyInfo.cs

--- BookOwnerTest/Properties/AssemblyInfo.cs
+++ BookOwnerTest/Properties/AssemblyInfo.cs
@@ -10,12 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-
-
 [assembly: Guid("4718c092-93ba-43b2-9ebd-ce741103ecdc")]
-
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
